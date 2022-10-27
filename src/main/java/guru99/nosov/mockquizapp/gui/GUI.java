f8c26766dfba114ff840f86a40cb5e5b71b154c3
Игорь Носов <NosovID@gmail.com>
2022-10-27 21:44:23 +0600

    Add Components and content to pane; need to arrange content and fix checkboxes to radiobutton's

--- src/main/java/guru99/nosov/mockquizapp/gui/GUI.java
+++ src/main/java/guru99/nosov/mockquizapp/gui/GUI.java
@@ -5,7 +5,25 @@ import java.awt.*;
 public class GUI {
 
     private static void addComponentsToPane(Container pane) {
+        pane.setLayout(new FlowLayout());
 
+        JLabel questionText = new JLabel("Que1: Which one among these is not datatype");
+        pane.add(questionText);
+
+        JRadioButtonMenuItem answerCheckBox1 = new JRadioButtonMenuItem("int");
+        pane.add(answerCheckBox1);
+
+        JRadioButtonMenuItem answerCheckBox2 = new JRadioButtonMenuItem("Float");
+        pane.add(answerCheckBox2);
+
+        JCheckBox answerCheckBox3 = new JCheckBox("boolean");
+        pane.add(answerCheckBox3);
+
+        JCheckBox answerCheckBox4 = new JCheckBox("char");
+        pane.add(answerCheckBox4);
+
+        JButton nextButton = new JButton("Next");
+        pane.add(nextButton);
 
     }
 
